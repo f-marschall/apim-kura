600cbe72aebd1b5c46060cdeee4c1c01e705058e
Felix <felix.mars@outlook.com>
2026-02-12 22:17:08 +0100

    update Terraform backend configuration to use environment-specific state key

--- test/tf/backend.tf
+++ test/tf/backend.tf
@@ -1,8 +1,4 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "apim-kura"
-    storage_account_name = "apimkura"
-    container_name       = "devops"
-    key                  = "apim.tfstate"
   }
 }
